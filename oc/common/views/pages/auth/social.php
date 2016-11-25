4a8ab805624668536d703487d2477fd7a0f8b282
amaloma <amaloma@gmail.com>
2016-11-25 23:41:19 +0700

    first commit

--- oc/common/views/pages/auth/social.php
+++ oc/common/views/pages/auth/social.php
@@ -1,14 +1,14 @@
 <?if (Theme::get('premium')!=1):?>
 <?//if (Theme::get('premium')==1):?>
     <?if (count($providers = Social::enabled_providers()) > 0) :?>
-        <br><br>
-     
+        <!-- <br><br> -->
+        <hr>
         <fieldset>
-            <legend><?=__('Social Login')?></legend>
-            <ul class="list-inline social-providers">
+            <legend align="center"><?=__('Social Login')?></legend>
+            <ul align="center" class="list-inline social-providers">
                 <?foreach ($providers as $key => $provider) :?>     
                     <?if(strtolower($key) == 'live')$key='windows'?>
-                   <li>
+                   <li >
                         <a class="zocial <?=strtolower($key)?> social-btn" href="<?=Route::url('default',array('controller'=>'social','action'=>'login','id'=>strtolower($key)))?>">
                             <?=$key?>
                         </a>
